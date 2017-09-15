8322d25fbabfa37518e200049a4ab2cf3b0df8b1
Renoir\Guido <guido.bag2.1@gmail.com>
2017-09-15 19:55:29 -0300

    Changes in the story system

--- Crystal Castle/Assets/Scripts/Script.cs
+++ Crystal Castle/Assets/Scripts/Script.cs
@@ -4,49 +4,16 @@ using UnityEngine;
 
 public class Script {
 
-
-	public string[] aoi1 = { 	"Nanny's castle",
-								"Think we made it in time?",
-								"WHA-",
-								"WHA-WHAT HAPPENED HERE???",
-								"And sis-",
-								"remember that paint you really liked?",
-								"1..",
-								"1.. 2..",
-								"1.. 2.. 3...",
-								"3 MILLIONS???",
-								"Yeah!",
-								"Let's kick him out ASAP."
-							};
-
-
-	public string[] saki1 = { 	"I don't know but,",
-								"let's hurry before the castle",
-								"starts stinking to him.",
-								"The Lord propably is on the last floor.",
-								"Come on, stupid brother.",
-								"The-",
-								"The-the monsters...",
-								"The-the monsters... ruined the carpets!!!",
-								"AHHHRRRGGGG!!!!!! NO!",
-								"not that paint...",
-								"It's from a famous french painter and",
-								"its value is 3 millions at least!",
-								"He's done it now.",
-								"No mercy for this barbaric dumb.",
-							};
-
-
-	public Enums.Character[] intro = 	{	
-											Enums.Character.Aoi, Enums.Character.Aoi,
-											Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
-											Enums.Character.Aoi, Enums.Character.Aoi,
-											Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
-											Enums.Character.Aoi, Enums.Character.Aoi,
-											Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
-											Enums.Character.Aoi, Enums.Character.Aoi, Enums.Character.Aoi, Enums.Character.Aoi,
-											Enums.Character.Saki, Enums.Character.Saki,
-											Enums.Character.Aoi, Enums.Character.Aoi
-										};
+	public static Enums.Character[] Intro = 	{	
+		Enums.Character.Aoi, Enums.Character.Aoi,
+		Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
+		Enums.Character.Aoi, Enums.Character.Aoi,
+		Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
+		Enums.Character.Aoi, Enums.Character.Aoi,
+		Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki, Enums.Character.Saki,
+		Enums.Character.Aoi, Enums.Character.Aoi, Enums.Character.Aoi, Enums.Character.Aoi,
+		Enums.Character.Saki, Enums.Character.Saki,
+		Enums.Character.Aoi, Enums.Character.Aoi
+	};
 
 }
---
+++ Crystal Castle/Assets/Scripts/StoryPart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StoryPart : MonoBehaviour {
+
+	public static StoryPart Instance;
+	protected int[] eventBreak;
+
+
+	private void Awake () {
+		if (Instance == null)
+			Instance = this;
+		else
+			Destroy (this);
+	}
+
+
+	protected virtual void OnAwake () {		}
+
+
+	private void Start () {
+		OnStart ();
+	}
+
+
+	protected virtual void OnStart () {		}
+
+
+	private void Update () {
+		OnUpdate ();
+	}
+
+
+	protected virtual void OnUpdate () {	}
+
+
+	public abstract void Init ();
+
+
+	public abstract void Next ();
+
+
+	public abstract void End ();
+
+
+	private void OnDestroy () {
+		if (Instance == this)
+			Instance = null;
+	}
+}
