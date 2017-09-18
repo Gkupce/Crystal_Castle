803d8688c884a1eefbe7e6b90e5d1cd8d107931a
gkupce <gkupce@gmail.com>
2017-09-17 23:39:20 -0300

    Started game controller to stop the player and enemies from moving in cinematics, level transitions or pause.

---
+++ Crystal Castle/Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	
+	public bool allowControll {
+		get {
+			return inCinematic || pause;
+		}
+	}
+
+	public bool pause = false;
+	public bool inCinematic = true;
+
+	private cinematics currentCinematic = cinematics.FadeIn;
+
+	public void StartCinematic(cinematics cinematicToStart)
+	{
+		inCinematic = true;
+		currentCinematic = cinematicToStart;
+		//TODO actually start cinematic
+	}
+
+	public void AfterCinematic()
+	{
+		if(currentCinematic == cinematics.FadeOut)
+		{
+			StartCinematic(cinematics.FadeIn);
+			currentCinematic = cinematics.FadeIn;
+		}
+		else
+		{
+			currentCinematic = cinematics.None;
+			inCinematic = false;
+		}
+	}
+
+
+	public enum cinematics
+	{
+		Start,
+		FadeIn,
+		FadeOut,
+		End,
+		GoingToStairs,
+		None
+	}
+}
