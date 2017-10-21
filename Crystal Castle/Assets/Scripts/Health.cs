e115847f35ab0eed65b1cf5293264fdc9fc70167
バニャスコ・ギード <guido.bag2.1@gmail.com>
2017-10-20 21:16:23 -0300

    Poison polishing that wasn't saved in the previous commit :/

--- Crystal Castle/Assets/Scripts/Health.cs
+++ Crystal Castle/Assets/Scripts/Health.cs
@@ -34,7 +34,7 @@ public class Health : MonoBehaviour {
 
 	IEnumerator Poisoned(float amount) {
 		while (health > 0f) {
-			yield return new WaitForSeconds (1f);
+			yield return new WaitForSeconds (0.7f);
 			ParticleManager.instance.EmitAt("Poison", transform.position, 7);
 			TakeDamage (amount);
 		}
