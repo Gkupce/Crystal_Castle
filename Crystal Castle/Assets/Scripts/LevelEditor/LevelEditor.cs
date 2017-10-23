6e5b7c9dbb60e41d99d99ed2f3f1f1eec4daa0ea
gkupce <gkupce@gmail.com>
2017-10-23 20:54:32 -0300

    Started custom tile editor

---
+++ Crystal Castle/Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class LevelEditor : MonoBehaviour {
+
+	public GameObject levelGO;
+	public GameObject currentTile;
+	public bool InEditMode;
+
+	public Vector3 pos;
+
+	//private void Awake()
+	//{
+	//	runInEditMode = true;
+	//}
+
+	// Update is called once per frame
+	void Update () {
+		pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pos.z = 0;
+		Debug.Log(pos);
+	}
+
+	//void OnDrawGizmos()
+	//{
+	//	Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+	//	Gizmos.color = Color.red;
+	//	Gizmos.DrawWireCube(pos, Vector3.one);
+	//}
+}
