a93d0859b9aef5cfdd40724893cacab0f26bc782
gkupce <gkupce@gmail.com>
2017-11-02 08:36:06 -0300

    recreated the level. I'm dumb.

---
+++ Crystal Castle/Assets/Scripts/Game master/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public Level[] levels;
+	public Level loadedLevel;
+
+	int currentLevel = 0;
+
+	public void LoadNextLevel()
+	{
+		if(loadedLevel != null)
+		{
+			GameObject.Destroy(loadedLevel);
+		}
+		loadedLevel = (Level)GameObject.Instantiate(levels[currentLevel]);
+		currentLevel++;
+	}
+}
