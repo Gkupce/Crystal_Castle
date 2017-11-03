905ac58a96443cfbde79eac7a5a751905c2f5729
el-pepi <el-pepi@live.com.ar>
2017-11-03 20:08:15 -0300

    +homing and bounce bombs

---
+++ Crystal Castle/Assets/Scripts/Weapons/SpreadBomb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadBomb : MonoBehaviour {
+
+    public GameObject projectile;
+    public string projectileName;
+
+    public void Explode(int gems,GemManager.GemType gemType)
+    {
+        int projectiles = gems * 2;
+        for (int i = 0; i < projectiles; i++)
+        {
+            GameObject b = Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, i * (360f / projectiles)));
+            //TODO: usar el nuevo pooling
+            b.SetActive(true);
+
+            switch (gemType) {
+                case GemManager.GemType.Homing:
+                    Homing h = b.GetComponent<Homing>();
+                    h.rotSpeed = 3f;
+                    h.maxAngleDif = 90f;
+                    h.enabled = true;
+                    break;
+                case GemManager.GemType.Bouncing:
+                    b.GetComponent<PlayerProjectile>().bounces = gems;
+                    break;
+            }
+        }
+    }
+}
