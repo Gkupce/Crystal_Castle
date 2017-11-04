1511f69ea3bb466d9ec2754191a19a153ab6bce0
バニャースコ　ギード <guido.bag2.1@gmail.com>
2017-11-04 11:27:38 -0300

    Deleted unused variables in Speed Gem

--- Crystal Castle/Assets/Scripts/Weapons/SpeedBombProjectile.cs
+++ Crystal Castle/Assets/Scripts/Weapons/SpeedBombProjectile.cs
@@ -5,12 +5,10 @@ using System;
 
 public class SpeedBombProjectile : PlayerProjectile {
 
-	float unit = 0.0f;
 	float freq = 2.0f;
 
 
 	public void Set(Transform player, int qty, int n) {
-		unit = 0.0f;
 		float angles = n * 360 / qty;
 		float thera = Mathf.Deg2Rad * angles;
 		transform.position = (Vector2)player.position + new Vector2 (Mathf.Cos (thera) * (thera > 270 || thera < 90 ? 1 : -1), Mathf.Sin (thera) * (thera < 180 ? 1 : -1));
@@ -21,6 +19,5 @@ public class SpeedBombProjectile : PlayerProjectile {
 	void Update () {
 		transform.Rotate(Vector3.forward, freq);
 		transform.Translate (Vector3.up * 1.3f * Time.deltaTime);
-		unit += Time.deltaTime;
 	}
 }
